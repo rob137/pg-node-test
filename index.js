382b4d980d4f8917703b19507779fc0156c45e2b
rob <robertaxelkirby@gmail.com>
2019-07-23 13:34:47 +0100

    Add parameterised insert query

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const { readUsers } = require('./routes/user');
+const { readUsers, insertUser } = require('./routes/user');
 
 const port = process.env.PORT || 5000;
 dotenv.config();
@@ -9,5 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => readUsers(req, res));
+
+app.post('/', (req, res) => insertUser(req, res));
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,18 @@ function readUsers(req, res) {
   });
 }
 
+function insertUser(req, res) {
+  const text = 'INSERT INTO users(email, admin) VALUES($1, $2)';
+  const { email, admin } = req.body;
+  const values = [email, admin];
+  db.query(text, values, (err, dbRes) => {
+    if (err) { return handleError(res, err); }
+    return res.send(dbRes);
+  });
+}
+
 function handleError(res, err) {
+  console.error(err);
   if (err.errno === 'ECONNREFUSED') {
     return res.status(500).send({ error: 'Unable to connect to database' });
   } else {
@@ -15,4 +26,4 @@ function handleError(res, err) {
   }
 }
 
-module.exports = { readUsers };
+module.exports = { readUsers, insertUser };
