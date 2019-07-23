6dc20587cd3890cca48c171c51b58aa7f4dd8f57
rob <robertaxelkirby@gmail.com>
2019-07-23 10:23:50 +0100

    Connect get handler to db

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { Client } = require('pg');
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 dotenv.config();
-const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+const app = express();
+app.use(express.json());
+
+const client = new Client({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'auth',
+  port: '5432'
+});
+
+app.get('/', async (req, res) => {
+  const users = await readUsers();
+  res.setHeader('content-type', 'application/json');
+  res.send(users);
 });
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
-})
+});
+
+connectToDb();
+
+async function connectToDb() {
+  try {
+    await client.connect();
+  }
+  catch(e) {
+    console.error(`Failed to connect ${e}`);
+  }
+}
+
+async function readUsers() {
+  try {
+    const response = await client.query('SELECT * FROM users');
+    return response.rows;
+  }
+  catch(e) {
+    return [];
+  }
+}
